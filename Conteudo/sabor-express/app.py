a9c2e45e6bcb643702dff8622ff9034796bffc03
Andre Cabalo <andreccabalo@gmail.com>
2023-12-21 21:37:29 -0300

    dia 21/12/23 atualizacoes

---
+++ Conteudo/sabor-express/app.py
@@ -0,0 +1,47 @@
+# Usamos o \n para pular uma linha antes e depois do Sabor Express
+import os
+
+def exibir_nome_do_programa():
+    print("""
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    """)
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair')
+
+def escolher_opcao():
+    # Agora pediremos um input, lembrando que ele deve ser inteiro
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    print(f'Você escolheu a opção: {opcao_escolhida}\n')
+
+    if opcao_escolhida == 1: 
+        print('Bora cadastrar')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def finalizar_app():
+    # Comando para limpar o terminal
+    os.system('cls')
+    print('Fechando aplicativo! volte sempre')
+
+#método que chama todos outros métodos
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+#condição que verifica se estamos na main, se estiver chama método main()
+if __name__ == '__main__':
+    main()
